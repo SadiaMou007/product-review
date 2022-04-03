314d66f3957e9b91849509008b981dfaeb3779ea
Your Name <you@example.com>
2022-04-03 08:37:05 -0700

    design page not found and home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="min-h-screen bg-slate-100">Home</div>;
+  return (
+    <div className="min-h-screen bg-slate-100">
+      <div className="grid md:grid-cols-2 pt-8 px-4">
+        <div className="p-4">
+          <h3>something</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+            repellendus quos adipisci saepe praesentium soluta corrupti
+            similique vitae ullam illo?
+          </p>
+          <button>Live Demo</button>
+        </div>
+        <div>
+          <img
+            src={
+              "https://img.freepik.com/free-psd/mockup-gaming-laptop-with-rgb-led-keyboard-glow_74058-461.jpg?size=626&ext=jpg&ga=GA1.1.1395737918.1641046705"
+            }
+            alt=""
+          ></img>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -3,13 +3,24 @@ import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
-    <div className="min-h-screen bg-yellow-100 text-bold  flex flex-col items-center pt-16">
-      <h3 className=" text-3xl text-red-600">Page Not Found!!!</h3>
-      <Link to={"/home"}>
-        <button className="bg-cyan-500 font-bold p-2 m-2 rounded text-cyan-900 text-xl border-2 hover:bg-cyan-200">
-          Go Back Home
-        </button>
-      </Link>
+    <div className="min-h-screen bg-yellow-100 text-bold p-8 grid md:grid-cols-2">
+      <div className="flex flex-col justify-center items-center">
+        <h3 className=" text-3xl text-red-600">Page Not Found!!!</h3>
+        <Link to={"/home"}>
+          <button className="bg-cyan-500 font-bold p-2 m-2 rounded text-cyan-900 text-xl border-2 hover:bg-cyan-200">
+            Go Back Home
+          </button>
+        </Link>
+      </div>
+      <div>
+        <img
+          src={
+            "https://img.freepik.com/free-photo/oops-awkward-confused-african-american-man-shrugging-shoulders-looking-worried-raising-empty-hands-up-dont-know-how-accident-happened-made-small-silly-mistake-white-background_176420-46751.jpg?size=626&ext=jpg&ga=GA1.2.1395737918.1641046705"
+          }
+          alt=""
+          className="rounded"
+        ></img>
+      </div>
     </div>
   );
 };
